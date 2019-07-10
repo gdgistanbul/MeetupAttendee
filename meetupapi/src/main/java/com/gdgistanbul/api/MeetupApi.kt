0f3bf36de192b2eebda129f1d8f98fde7d440166
Cafer Mert Ceyhan <mert.ceyhan@arabam.com>
2019-07-10 15:55:40 +0300

    Update path name as like meetup api documentation, add missing parameters and delete unnecessary @JSON annotation

--- meetupapi/src/main/java/com/gdgistanbul/api/MeetupApi.kt
+++ meetupapi/src/main/java/com/gdgistanbul/api/MeetupApi.kt
@@ -7,16 +7,21 @@ import retrofit2.http.*
 interface MeetupApi {
 
     @GET("{url_name}/events")
-    suspend fun getEvents(@Path("url_name") group: String = "GDGIstanbul"): List<Event>
+    suspend fun getEvents(@Path("url_name") urlName: String = "GDGIstanbul"): List<Event>
 
     @GET("{url_name}/events/{id}/attendance")
     suspend fun getAttendees(
-        @Path("group") group: String = "GDGIstanbul",
+        @Path("url_name") urlName: String = "GDGIstanbul",
         @Path("id") id: String,
         @Query("order") order: String = "name"
     ): List<Attendance>
 
 
     @POST("{url_name}/events/{id}/attendance")
-    suspend fun getAttendenceTaking(@Field("member") id: String, @Field("status") status: String)
+    suspend fun getAttendenceTaking(
+        @Path("url_name") urlName: String = "GDGIstanbul",
+        @Path("id") id: String,
+        @Field("member") memberId: String,
+        @Field("status") status: String
+    )
 }
--- model/src/main/java/com/gdgistanbul/model/Model.kt
+++ model/src/main/java/com/gdgistanbul/model/Model.kt
@@ -5,135 +5,92 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Event(
-    @Json(name = "created")
     val created: Long?,
-    @Json(name = "duration")
     val duration: Int?,
-    @Json(name = "id")
     val id: String?,
-    @Json(name = "name")
     val name: String?,
-    @Json(name = "rsvp_limit")
-    val rsvpLimit: Int?,
-    @Json(name = "date_in_series_pattern")
-    val dateInSeriesPattern: Boolean?,
-    @Json(name = "status")
     val status: String?,
-    @Json(name = "time")
     val time: Long?,
+    val venue: Venue?,
+    val group: Group?,
+    val link: String?,
+    val updated: Long?,
+    val visibility: String?,
+    val description: String?,
     @Json(name = "local_date")
     val localDate: String?,
     @Json(name = "local_time")
     val localTime: String?,
-    @Json(name = "updated")
-    val updated: Long?,
+    @Json(name = "rsvp_limit")
+    val rsvpLimit: Int?,
+    @Json(name = "how_to_find_us")
+    val howToFindUs: String?,
+    @Json(name = "date_in_series_pattern")
+    val dateInSeriesPattern: Boolean?,
     @Json(name = "utc_offset")
     val utcOffset: Int?,
     @Json(name = "waitlist_count")
     val waitlistCount: Int?,
     @Json(name = "yes_rsvp_count")
-    val yesRsvpCount: Int?,
-    @Json(name = "venue")
-    val venue: Venue?,
-    @Json(name = "group")
-    val group: Group?,
-    @Json(name = "link")
-    val link: String?,
-    @Json(name = "description")
-    val description: String?,
-    @Json(name = "how_to_find_us")
-    val howToFindUs: String?,
-    @Json(name = "visibility")
-    val visibility: String?
+    val yesRsvpCount: Int?
 )
 
 @JsonClass(generateAdapter = true)
 data class Venue(
-    @Json(name = "id")
     val id: Int?,
-    @Json(name = "name")
     val name: String?,
-    @Json(name = "lat")
     val lat: Double?,
-    @Json(name = "lon")
     val lon: Double?,
-    @Json(name = "repinned")
     val repinned: Boolean?,
+    val city: String?,
+    val country: String?,
     @Json(name = "address_1")
     val address1: String?,
-    @Json(name = "city")
-    val city: String?,
-    @Json(name = "country")
-    val country: String?,
     @Json(name = "localized_country_name")
     val localizedCountryName: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Group(
-    @Json(name = "created")
     val created: Long?,
-    @Json(name = "name")
     val name: String?,
-    @Json(name = "id")
     val id: Int?,
-    @Json(name = "join_mode")
     val joinMode: String?,
-    @Json(name = "lat")
     val lat: Double?,
-    @Json(name = "lon")
     val lon: Double?,
-    @Json(name = "urlname")
     val urlname: String?,
-    @Json(name = "who")
     val who: String?,
-    @Json(name = "localized_location")
-    val localizedLocation: String?,
-    @Json(name = "state")
     val state: String?,
-    @Json(name = "country")
     val country: String?,
-    @Json(name = "region")
     val region: String?,
-    @Json(name = "timezone")
-    val timezone: String?
+    val timezone: String?,
+    @Json(name = "localized_location")
+    val localizedLocation: String?
 )
 
 @JsonClass(generateAdapter = true)
 data class Attendance(
-    @Json(name = "member")
     val member: Member?,
-    @Json(name = "rsvp")
     val rsvp: Rsvp?,
-    @Json(name = "attendance_id")
-    val attendanceId: Int?,
-    @Json(name = "status")
     val status: String?,
-    @Json(name = "updated")
     val updated: Long?,
-    @Json(name = "guests")
-    val guests: Int?
+    val guests: Int?,
+    @Json(name = "attendance_id")
+    val attendanceId: Int?
 )
 
 @JsonClass(generateAdapter = true)
 data class Rsvp(
-    @Json(name = "id")
     val id: Int?,
-    @Json(name = "response")
     val response: String?,
-    @Json(name = "guests")
     val guests: Int?,
-    @Json(name = "updated")
     val updated: Long?
 )
 
 @JsonClass(generateAdapter = true)
 data class Member(
-    @Json(name = "id")
     val id: Int?,
-    @Json(name = "name")
     val name: String?,
-    @Json(name = "photo")
     val photo: Photo?,
     @Json(name = "event_context")
     val eventContext: EventContext?
@@ -141,22 +98,19 @@ data class Member(
 
 @JsonClass(generateAdapter = true)
 data class EventContext(
-    @Json(name = "host")
     val host: Boolean?
 )
 
 @JsonClass(generateAdapter = true)
 data class Photo(
-    @Json(name = "id")
     val id: Int?,
+    val type: String?,
     @Json(name = "highres_link")
     val highresLink: String?,
     @Json(name = "photo_link")
     val photoLink: String?,
     @Json(name = "thumb_link")
     val thumbLink: String?,
-    @Json(name = "type")
-    val type: String?,
     @Json(name = "base_url")
     val baseUrl: String?
 )
