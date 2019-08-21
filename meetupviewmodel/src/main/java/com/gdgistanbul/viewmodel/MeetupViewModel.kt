404204dfc0ea101bc17e99a4f4dca5886d9af5d3
perihanimamoglu <perihanimamoglu1@gmail.com>
2019-08-21 23:40:05 +0300

    Add accessTokenLiveData

--- meetupviewmodel/src/main/java/com/gdgistanbul/viewmodel/MeetupViewModel.kt
+++ meetupviewmodel/src/main/java/com/gdgistanbul/viewmodel/MeetupViewModel.kt
@@ -5,14 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gdgistanbul.model.Event
+import com.gdgistanbul.model.Login
 import com.gdgistanbul.repo.MeetupRepo
 import kotlinx.coroutines.launch
 
 class MeetupViewModel(private val meetupRepo: MeetupRepo) : ViewModel() {
 
-
     val eventsLiveData = MutableLiveData<List<Event>>()
     val toastLiveData = MutableLiveData<String>()
+    val accessTokenLiveData = MutableLiveData<Login>()
 
     fun refreshEvents() = viewModelScope.launch {
         try {
@@ -23,6 +24,13 @@ class MeetupViewModel(private val meetupRepo: MeetupRepo) : ViewModel() {
         }
     }
 
-
+    fun getAccessToken(code: String) = viewModelScope.launch {
+        try {
+            accessTokenLiveData.postValue(meetupRepo.getAccessToken(code))
+        } catch (e: Exception) {
+            toastLiveData.postValue(e.message)
+            Log.d("meetupviewmodel", e.message, e)
+        }
+    }
 }
 
